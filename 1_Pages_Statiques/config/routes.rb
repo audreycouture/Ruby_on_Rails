dbe4334cda1a879081b11a2d05a399bd6921fa63
audreycouture <38008305+audreycouture@users.noreply.github.com>
2018-04-27 08:37:34 +0200

    Update routes.rb

--- 1_Pages_Statiques/config/routes.rb
+++ 1_Pages_Statiques/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
-  
+  get '/home', to: "static_pages#home"
   get '/contact', to: "static_pages#contact"
   get '/about', to: "static_pages#about"
   get '/about/jerome', to: "static_pages#about_jerome", as: "jerome"
